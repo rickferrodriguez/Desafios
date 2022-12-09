db1787aab3ee13705933ec7009a231bfda901dce
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-12-09 17:34:52 -0500

    mas tarde continuamos

--- JavaClass/calculadora/js/app.js
+++ JavaClass/calculadora/js/app.js
@@ -1,1 +1,9 @@
-console.log('Aplicación calculadora');
+// console.log('Aplicación calculadora');
+function sumar(){
+    const forma = document.forms['forma'];
+    let a =forma.elements.operandoA.getValue;
+    let b = forma.elements.operandoB.getValue;
+    let total = a + b;
+    console.log(`El total es:${total}`)
+    return total;
+}
