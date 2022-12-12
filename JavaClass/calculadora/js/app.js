844a8b19fffad460e2cea6a8aaa31d331ef5ca7b
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-12-12 10:11:27 -0500

    arreglos de interfaz y cositas de JavaScript

--- JavaClass/calculadora/js/app.js
+++ JavaClass/calculadora/js/app.js
@@ -1,70 +1,44 @@
 // console.log('Aplicación calculadora');
 function numerosText(numero){
-    let texto = document.getElementById('elTexto');
+    let texto = document.getElementById('input-texto');
     const numerosBot = [1,2,3,4,5,6,7,8,9,0];
     for (let i = 0; i < numerosBot.length; i++) {
         let elNumero = numerosBot[i];
         if(numero == elNumero){
 
-            texto.value = texto.value + (numero);
+            return texto.value = texto.value + (numero);
         }
     }
-    // if(numero == 1){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 2){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 3){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 4){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 5){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 6){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 7){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 8){
-    //     texto.value = texto.value + (numero);
-    // }
-    // else if(numero == 9){
-    //     texto.value = texto.value + (numero);
-    // }
     console.log(texto);
 }
 function operacion(vari){
     const forma = document.forms['forma'];
-    let operandoA =forma['operandoA'], operandoB =forma['operandoB'];
+    let operandoB =forma['operando-b'];
+    let texto = document.getElementById('input-texto');
     let result = document.getElementById('resultado');
     let total = 0;
     if(vari == '+'){
-        total = parseInt(operandoA.value) +parseInt(operandoB.value); 
+        total = parseInt(texto.value) +parseInt(operandoB.value); 
         total =result.innerHTML= `El total es: ${total}`;
     }
     else if(vari == '-'){
-        total = parseInt(operandoA.value) -parseInt(operandoB.value); 
+        total = parseInt(texto.value) -parseInt(operandoB.value); 
         total =result.innerHTML= `El total es: ${total}`;
     }
     else if(vari == '*'){
-        total = parseInt(operandoA.value) * parseInt(operandoB.value); 
+        total = parseInt(texto.value) * parseInt(operandoB.value); 
         total =result.innerHTML= `El total es: ${total}`;
     }
     else{
-        total = parseInt(operandoA.value) / parseInt(operandoB.value); 
+        total = parseInt(texto.value) / parseInt(operandoB.value); 
         total =result.innerHTML= `El total es: ${total}`;
     }
     
-    try{
-        if (operandoA.value == '') throw 'El campo "valor A" se encuentra vacio';
-        if (operandoB.value == '') throw 'El campo "valor B" se encuentra vacio';
-    }catch(err){
-        total = result.innerHTML = err;
-    }
+    // try{
+    //     if (operandoA.value == '') throw 'El campo "valor A" se encuentra vacio';
+    //     if (operandoB.value == '') throw 'El campo "valor B" se encuentra vacio';
+    // }catch(err){
+    //     total = result.innerHTML = err;
+    // }
     console.log(total);
 }
