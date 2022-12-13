8ec11ce0c3ae36b7c7a5baf13758c6922dfa7c8d
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-12-13 10:14:08 -0500

    los operadores funcionando, resolver el igual

--- JavaClass/calculadora/js/app.js
+++ JavaClass/calculadora/js/app.js
@@ -1,35 +1,69 @@
 // console.log('Aplicación calculadora');
 
 const sumado = [];
+// sirve para saber si voy a sumar algo mas o no
+const orden = [];
 let total = 0;
 function numerosText(numero){
     let texto = document.getElementById('input-texto');
-    return texto.value = texto.value + (numero);
+    if(sumado.length > 0 && texto.value != '' && orden.length > 0){
+        texto.value = numero;
+        // significa que estoy esperando una suma y necesito remplazar el texto con el total para realizar una operación nuevamente
+        orden.splice(0,1);
+        console.log(orden);
+    }
+    else
+        return texto.value = texto.value + (numero);
     // console.log(texto);
 }
 function operacion(vari){
     let texto = document.getElementById('input-texto');
-    if(vari == '+'){
+    if(sumado.length == 0){
         sumado.push(texto.value)
         texto.value = '';
         console.log(sumado)
     }
-    else if(vari == '-'){
-        // total = parseInt(texto.value) -parseInt(operandoB.value); 
-        // total =result.innerHTML= `El total es: ${total}`;
-    }
-    else if(vari == '*'){
-        // total = parseInt(texto.value) * parseInt(operandoB.value); 
-        // total =result.innerHTML= `El total es: ${total}`;
-    }
-    else if(vari == '='){
+    else if(sumado.length > 0){
         for(let i = 0; i < sumado.length; i++){
             let valor = sumado[i];
-            total = parseInt(total)+ parseInt(valor) ;
-            texto.value = total;
+            if (vari == '+')
+            {
+                total = parseInt(valor) + parseInt(texto.value); 
+                texto.value = total;
+                sumado.splice(0,1, total);
+                orden.push(vari);
+                console.log(sumado);
+            }
+            else if (vari == '-')
+            {
+                total = parseInt(valor) - parseInt(texto.value); 
+                texto.value = total;
+                sumado.splice(0,1, total);
+                orden.push(vari);
+                console.log(sumado);
+            }
+            else if (vari == '*')
+            {
+                total = parseInt(valor) * parseInt(texto.value); 
+                texto.value = total;
+                sumado.splice(0,1, total);
+                orden.push(vari);
+                console.log(sumado);
+            }
+            else if (vari == '/')
+            {
+                total = parseInt(valor) / parseInt(texto.value); 
+                texto.value = total;
+                sumado.splice(0,1, total);
+                orden.push(vari);
+                console.log(sumado);
+            }
         }
-        // total = parseInt(texto.value) * parseInt(operandoB.value); 
-        // total =result.innerHTML= `El total es: ${total}`;
+    }
+    else if(vari == '='){
+            total;
+            texto.value = total;
+            sumado.splice(0)
     }
     else{
         // total = parseInt(texto.value) / parseInt(operandoB.value); 
