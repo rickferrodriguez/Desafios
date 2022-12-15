4366af2f5c15036b657ce853d10927fb4d7320c4
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-12-15 14:59:49 -0500

    working with map

--- JavaClass/calculadora/js/app.js
+++ JavaClass/calculadora/js/app.js
@@ -1,17 +1,20 @@
 // console.log('Aplicación calculadora');
-
 let sumado = [
     {valor1: 0,valor2: 0}
 ];
 let valor1 = sumado.map(function(suma){
     return parseInt(suma.valor1);
 });
+
+let valor2 = sumado.map(function(suma){
+    return parseInt(suma.valor2);
+});
 // porque necesito saber si voy a continuar con la operación o no
 const orden = [];
 let total = 0;
 function numerosText(numero){
     let texto = document.getElementById('input-texto');
-    if(sumado.length > 0 && texto.value != '' && orden.length > 0){
+    if(!valor1 == 0 && texto.value != '' && orden.length > 0){
         texto.value = numero;
         // necesito cambiar su valor para continuar escribiendo nuevos numeros
         orden.splice(0,1);
@@ -29,72 +32,70 @@ function operacion(vari){
         texto.value = '';
         console.log(`este es mi valor 1: ${valor1}`);
     }
-    else if(sumado.valor2 == 0){
-        let elValor = sumado.map(function(valor){
-            if (vari == '+'){
-                orden.push(vari);
-                total = parseInt(valor) + parseInt(texto.value); 
-                texto.value = total;
-                sumado.splice(0,1, total);
-                console.log(sumado);
-            }
-            else if (vari == '-'){
-                total = parseInt(valor) - parseInt(texto.value); 
-                texto.value = total;
-                sumado.splice(0,1, total);
-                orden.push(vari);
-                console.log(sumado);
-            }
-            else if (vari == '*'){
-                total = parseInt(valor) * parseInt(texto.value); 
-                texto.value = total;
-                sumado.splice(0,1, total);
-                orden.push(vari);
-                console.log(sumado);
-            }
-            else if (vari == '/'){
-                total = parseInt(valor) / parseInt(texto.value); 
-                texto.value = total;
-                sumado.splice(0,1, total);
-                orden.push(vari);
-                console.log(sumado);
-            }
-            else if(vari == '='){
-                console.log(orden)
-                for(let i = 0; i < sumado.length; i++){
-                    let valor = sumado[i];
-                    if (orden == '+')
-                    {
-                        total = parseInt(valor) + parseInt(texto.value); 
-                        texto.value = total;
-                        sumado.splice(0,1, 0);
-                        console.log(sumado);
-                    }
-                    else if (orden == '-')
-                    {
-                        total = parseInt(valor) - parseInt(texto.value); 
-                        texto.value = total;
-                        sumado.splice(0,1, total);
-                        console.log(sumado);
-                    }
-                    else if (orden == '*')
-                    {
-                        total = parseInt(valor) * parseInt(texto.value); 
-                        texto.value = total;
-                        sumado.splice(0,1, total);
-                        console.log(sumado);
-                    }
-                    else if (orden == '/')
-                    {
-                        total = parseInt(valor) / parseInt(texto.value); 
-                        texto.value = total;
-                        sumado.splice(0,1, total);
-                        console.log(sumado);
-                    }
+    else if(valor2 == 0){
+        if (vari == '+'){
+            orden.push(vari);
+            total = parseInt(valor1) + parseInt(texto.value); 
+            texto.value = total;
+            valor1.splice(0,1, total);
+            console.log(sumado);
+        }
+        else if (vari == '-'){
+            orden.push(vari);
+            total = parseInt(valor1) - parseInt(texto.value); 
+            texto.value = total;
+            valor1.splice(0,1, total);
+            console.log(sumado);
+        }
+        else if (vari == '*'){
+            total = parseInt(valor1) * parseInt(texto.value); 
+            texto.value = total;
+            valor1.splice(0,1, total);
+            orden.push(vari);
+            console.log(sumado);
+        }
+        else if (vari == '/'){
+            total = parseInt(valor1) / parseInt(texto.value); 
+            texto.value = total;
+            valor1.splice(0,1, total);
+            orden.push(vari);
+            console.log(sumado);
+        }
+        else if(vari == '='){
+            console.log(orden)
+            for(let i = 0; i < sumado.length; i++){
+                let valor = sumado[i];
+                if (orden == '+')
+                {
+                    total = parseInt(valor) + parseInt(texto.value); 
+                    texto.value = total;
+                    sumado.splice(0,1, 0);
+                    console.log(sumado);
                 }
-                console.log(elValor);
+                else if (orden == '-')
+                {
+                    total = parseInt(valor) - parseInt(texto.value); 
+                    texto.value = total;
+                    sumado.splice(0,1, total);
+                    console.log(sumado);
+                }
+                else if (orden == '*')
+                {
+                    total = parseInt(valor) * parseInt(texto.value); 
+                    texto.value = total;
+                    sumado.splice(0,1, total);
+                    console.log(sumado);
+                }
+                else if (orden == '/')
+                {
+                    total = parseInt(valor) / parseInt(texto.value); 
+                    texto.value = total;
+                    sumado.splice(0,1, total);
+                    console.log(sumado);
+                }
             }
-        });
+            console.log(elValor);
+        }
     }
 
     else{
