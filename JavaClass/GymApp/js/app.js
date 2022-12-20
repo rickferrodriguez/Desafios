0747d0145a35d96a6e001b6167056fb0d24ba470
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-12-19 23:08:48 -0500

    adding new images and changing stuff

--- JavaClass/GymApp/js/app.js
+++ JavaClass/GymApp/js/app.js
@@ -1,6 +1,7 @@
 const exercises = [
-    new Exercise('Legs Extensions','images/legex.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps'),
-    new Exercise('Bench Press','images/legex.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps'),
+    new Exercise('Legs Extensions','images/squat.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps'),
+    new Exercise('Bench Press','images/deadlift.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps'),
+    new Exercise('Pull ups','images/benchpress.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps'),
 ];
 
 function showExercises(){
@@ -24,13 +25,12 @@ function showExercises(){
         const d_time = document.createElement('div');
 
         // agregar clases a los elementos
-        row.classList.add('row');
         col.classList.add('col','s12','m7');
         carta.classList.add('card');
         d_nombre.classList.add('card-title','grey-text','text-darken-4');
         div_img_element.classList.add('card-image');
         img_element.classList.add('responsive-img');
-        div_desc_element.classList.add('card-content');
+        div_desc_element.classList.add('card-content', 'grey');
 
         // Asigno el contenido a través de la propiedad `textContent`
         d_nombre.textContent = eNombre;
@@ -41,8 +41,7 @@ function showExercises(){
 
         // Inserto el elemento en el DOM
         let contenedora = document.getElementById("container");
-        contenedora.appendChild(row);
-        row.appendChild(col);
+        contenedora.appendChild(col);
         col.appendChild(carta);
         carta.appendChild(div_img_element);
         div_img_element.appendChild(img_element);
