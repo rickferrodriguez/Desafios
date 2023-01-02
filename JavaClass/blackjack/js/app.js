033c5d04e5d94bc7827119a4727dc765e4c3a4f8
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-02 12:55:16 -0500

    finally finished the image loader, requiring more changes for later

--- JavaClass/blackjack/js/app.js
+++ JavaClass/blackjack/js/app.js
@@ -1,18 +1,18 @@
 let cards = [];
 let cartas = [
-    { number: 1, imageUrl: 'images/cards1'},
-    { number: 2, imageUrl: 'images/cards1'},
-    { number: 3, imageUrl: 'images/cards1'},
-    { number: 4, imageUrl: 'images/cards1'},
-    { number: 5, imageUrl: 'images/cards1'},
-    { number: 6, imageUrl: 'images/cards1'},
-    { number: 7, imageUrl: 'images/cards1'},
-    { number: 8, imageUrl: 'images/cards1'},
-    { number: 9, imageUrl: 'images/cards1'},
-    { number: 10, imageUrl: 'images/cards1'},
-    { number: 11, imageUrl: 'images/cards1'},
-    { number: 12, imageUrl: 'images/cards1'},
-    { number: 13, imageUrl: 'images/cards1'}
+    { number: 11, imageUrl: 'images/1.png'},
+    { number: 2, imageUrl: 'images/2.png'},
+    { number: 3, imageUrl: 'images/3.png'},
+    { number: 4, imageUrl: 'images/4.png'},
+    { number: 5, imageUrl: 'images/5.png'},
+    { number: 6, imageUrl: 'images/6.png'},
+    { number: 7, imageUrl: 'images/7.png'},
+    { number: 8, imageUrl: 'images/8.png'},
+    { number: 9, imageUrl: 'images/9.png'},
+    { number: 10, imageUrl: 'images/10.png'},
+    { number: 10, imageUrl: 'images/j.png'},
+    { number: 10, imageUrl: 'images/q.png'},
+    { number: 10, imageUrl: 'images/k.png'},
 ]
 let sum = 0;
 let isAlive = false;
@@ -37,10 +37,10 @@ function startGame(){
     isAlive = true;
     hasBlackJack = false;
     // iniciar las variables de las dos cartas dentro de esta función para que el juego no venga con cartas por defecto al iniciar
-    let firstCard = getRandomNumber();
-    let secondCard = getRandomNumber();
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
     cards = [firstCard, secondCard];
-    sum = firstCard + secondCard;
+    sum = firstCard.number + secondCard.number;
     renderGame();
 }
 
@@ -48,9 +48,14 @@ function renderGame(){
     // necesito este texto por defecto al momento de llamar al juego
     $cards.textContent = "Cards: ";
     $sum.textContent = `Sum: ${sum}`;
-    $contcards.textContent = `cont de cartas: ${cartas.number}`
+    $contcards.textContent = ``
     for (let i = 0; i < cards.length; i++) {
-        $cards.textContent += `${cards[i]} `;
+        $cards.textContent += `${cards[i].number} `;
+        let cont = document.getElementById("cont-cartas");
+        const img_element = document.createElement('img')
+        img_element.classList.add('img-card');
+        img_element.src = cards[i].imageUrl;
+        cont.appendChild(img_element);
     }
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂";
@@ -66,23 +71,29 @@ function renderGame(){
 
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
-        let theCard = getRandomNumber();
-        sum += theCard;
+        let theCard = getRandomCard();
+        sum += theCard.number;
         cards.push(theCard);
         renderGame();
     } 
 }
 
-function getRandomNumber() {
-    // Math.floor : removes the decimals
-    // Math.random : returns random number between 0 and 13
-    let random =Math.floor(Math.random() * 13)+ 1;
-    if (random > 10) {
-        return 10
-    } else if (random === 1) {
-        return 11
-    } else {
-        return random
-    }
-    // con el '+1' se le indica que va a empezar desde el uno, pero en realidad se le está sumando un valor al generador de numeros
+// function getRandomNumber() {
+//     // Math.floor : removes the decimals
+//     // Math.random : returns random number between 0 and 13
+//     let random =Math.floor(Math.random() * 13)+ 1;
+//     if (random > 10) {
+//         return 10
+//     } else if (random === 1) {
+//         return 11
+//     } else {
+//         return random
+//     }
+//     // con el '+1' se le indica que va a empezar desde el uno, pero en realidad se le está sumando un valor al generador de numeros
+// }
+
+function getRandomCard() {
+    let random = Math.floor(Math.random() * cartas.length)
+    let miCarta = cartas[random]
+    return miCarta
 }
