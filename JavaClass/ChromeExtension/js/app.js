3ff01ed6a9d09de814fe9a3dc959152c768a1255
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-06 15:02:59 -0500

    creating the chrome extension

---
+++ JavaClass/ChromeExtension/js/app.js
@@ -0,0 +1,14 @@
+
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const $input_el = document.getElementById('input-el');
+const $input_btn = document.getElementById('input-btn');
+const $ul_el = document.getElementById('ul-el');
+
+$input_btn.addEventListener("click", function (){
+    myLeads.push($input_el.value);
+    console.log(myLeads);
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    $ul_el.textContent += `${myLeads[i]} `;
+}
