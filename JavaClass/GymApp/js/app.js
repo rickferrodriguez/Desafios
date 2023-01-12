1fa34e9222e04bac32aff18414af45bde31e8e30
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-12 15:38:37 -0500

    changes i guess

--- JavaClass/GymApp/js/app.js
+++ JavaClass/GymApp/js/app.js
@@ -17,7 +17,7 @@ function showExercises(){
                             <span class="card-title grey-text text-darken-4">${exercise.nombre}</span>
                         </div>
                         <div class="card-content">
-                            <p>${exercise.description}</p>
+                            <p >${exercise.description}</p>
                         </div>
                         <div class="card-action">
                             <p>Calories: ${exercise.calories}</p>
