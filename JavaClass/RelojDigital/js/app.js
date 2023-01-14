49b01902758f51a17bce666d4ad04d1f0476faaa
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-13 23:18:40 -0500

    reloj digital terminado, practicar con esto de las fechas para la aplicación del gimnasio

--- JavaClass/RelojDigital/js/app.js
+++ JavaClass/RelojDigital/js/app.js
@@ -1,10 +1,30 @@
 const $hora = document.getElementById('hora')
+const $fecha = document.getElementById('fecha')
+const $contenedor = document.getElementById('contenedor')
+
 const mostrarReloj = () => {
     let fecha = new Date()
-    let hr = fecha.getHours()
-    let min = fecha.getMinutes()
-    let seg = fecha.getSeconds()
+    let hr = formatoHora(fecha.getHours())
+    let min = formatoHora(fecha.getMinutes())
+    let seg = formatoHora(fecha.getSeconds())
     $hora.innerHTML = `${hr}:${min}:${seg}`
+
+    const meses = ['Ene', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    const dias = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Sab']
+    let diaSemana = dias[fecha.getDay()]
+    let dia = fecha.getDate()
+    let mes = meses[fecha.getMonth()]
+    let fechaTexto = `${diaSemana}, ${dia} ${mes}`
+    $fecha.innerHTML = fechaTexto
+    $contenedor.classList.toggle('animar')
+}
+
+// agregando formato a los numeros
+const formatoHora = (hora) => {
+    if(hora < 10){
+        hora = '0' + hora;
+    }
+    return hora;
 }
 
 // sirve para llamar a una función y que esta se ejecute cada ciertos milisegundos
