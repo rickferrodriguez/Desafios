330aea32f7521f960b42fb706c8bb4328b230fe1
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-15 17:27:06 -0500

    pushing an object inside the ingresos array

--- JavaClass/Presupuesto/js/app.js
+++ JavaClass/Presupuesto/js/app.js
@@ -2,9 +2,6 @@ const $pre_val = document.getElementById('pre-val')
 const $ingre_val = document.getElementById('ingre-val')
 const $egre_val = document.getElementById('egre-val')
 const $percent_val = document.getElementById('percent-val')
-const $input_selector = document.getElementById('selector')
-const $input_desc_el = document.getElementById('desc-el')
-const $input_valor_el = document.getElementById('valor-el')
 const $add_btn = document.getElementById('add-btn')
 const $inner_ing = document.getElementById('inner-ing')
 const $inner_egre = document.getElementById('inner-egre')
@@ -14,7 +11,14 @@ const ingresos = []
 const egresos = []
 
 $add_btn.addEventListener('click', () => {
-    alert($input_selector.selectedIndex)
+    const $input_selector = document.getElementById('selector').selectedIndex
+    const $input_desc = document.getElementById('desc-el').getValue()
+    const $input_valor = document.getElementById('valor-el').getValue()
+    if($input_selector === 0 && $input_desc != '' && $input_valor != ''){
+        let ing_val = { desc: $input_desc, valor: $input_valor}
+        ingresos.push(ing_val)
+        alert($input_selector.selectedIndex)
+    }
 })
 
 $pre_val.innerHTML = `$ ${presupuesto.toFixed(2)}`
