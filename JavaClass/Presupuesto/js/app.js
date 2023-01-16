4354e594c3edf4c1d42d57f55f7d4ebd33eb1e4a
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-15 21:37:01 -0500

    finally rendering the both value with function with parameters

--- JavaClass/Presupuesto/js/app.js
+++ JavaClass/Presupuesto/js/app.js
@@ -5,6 +5,8 @@ const $percent_val = document.getElementById('percent-val')
 const $add_btn = document.getElementById('add-btn')
 const $inner_ing = document.getElementById('inner-ing')
 const $inner_egre = document.getElementById('inner-egre')
+const ingre_colors = 'teal-text text-lighten-2'
+const egre_colors = 'red-text text-lighten-1'
 
 let presupuesto = 2300
 const ingresos = []
@@ -17,25 +19,24 @@ $add_btn.addEventListener('click', () => {
     if($input_selector === 0 && $input_desc != '' && $input_valor != ''){
         let ing_val = { desc: $input_desc, valor: $input_valor}
         ingresos.push(ing_val)
-        console.log(ingresos)
-        render(ingresos)
+        render(ingresos, $inner_ing, ingre_colors)
     } else if ($input_selector === 1 && $input_desc != '' && $input_valor != '') {
         let egre_val = { desc: $input_desc, valor: $input_valor}
         egresos.push(egre_val)
-        console.log(egresos)
+        render(egresos, $inner_egre, egre_colors)
     } else {
-    alert('Por favor ingrese los campos')
+    alert('Por favor diligencie los campos')
     }
 })
 
-const render = (arr) => {
-    let text = '<li class="collection-header"><h3 class="teal-text text-lighten-2">INGRESOS</h3></li>'
-    arr.map( ingreso  =>{
+const render = (arr, inner, color) => {
+    let text = `<li class="collection-header"><h3 class="${color}">INGRESOS</h3></li>`
+    arr.map( value  =>{
         text += `
-        <li class="collection-item"><div>${ingreso.desc}<a href="#!" class="secondary-content">${ingreso.valor}</a></div></li>
+        <li class="collection-item"><div>${value.desc}<a href="#!" class="secondary-content">${value.valor}</a></div></li>
     `
     })
-    $inner_ing.innerHTML = text
+    inner.innerHTML = text
 }
 
 $pre_val.innerHTML = `$ ${presupuesto.toFixed(2)}`
