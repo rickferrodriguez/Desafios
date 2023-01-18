43b8988e005e3869831a81347c1427b894c254aa
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-18 16:08:21 -0500

    using prettier

--- JavaClass/GymApp/js/app.js
+++ JavaClass/GymApp/js/app.js
@@ -1,74 +1,96 @@
 const exercises = [
-    new Exercise('Squat','images/squat.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps', 'legs'),
-    new Exercise('Dead Lift','images/deadlift.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps', 'legs'),
-    new Exercise('Bench Press','images/benchpress.jpg','The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.','400','4reps', 'chest'),
+    new Exercise(
+        "Squat",
+        "images/squat.jpg",
+        "The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.",
+        "400",
+        "4reps",
+        "legs"
+    ),
+    new Exercise(
+        "Dead Lift",
+        "images/deadlift.jpg",
+        "The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.",
+        "400",
+        "4reps",
+        "legs"
+    ),
+    new Exercise(
+        "Bench Press",
+        "images/benchpress.jpg",
+        "The exercise works mainly the quadriceps muscles of the front of the thigh—the rectus femoris and the vastusmuscles.",
+        "400",
+        "4reps",
+        "chest"
+    ),
 ];
 
 const exercises2 = {
-    pierna : [ {nombre: 'Squat', desc: 'Squad Desc'},
-            {nombre: 'Racks', desc: 'Racks Desc'}],
-    chest : [ {nombre: 'press', desc: 'press Desc'},
-            {nombre: 'press2', desc: 'press2 Desc'}]
-}
+    pierna: [
+        { nombre: "Squat", desc: "Squad Desc" },
+        { nombre: "Racks", desc: "Racks Desc" },
+    ],
+    chest: [
+        { nombre: "press", desc: "press Desc" },
+        { nombre: "press2", desc: "press2 Desc" },
+    ],
+};
 
-const recorrerEjercicios = (ejer) =>{
-    let eje = (exercises2[ejer]) ? 
-        exercises2[ejer].map( exer =>{
-        return exer.nombre
-        })
-        :
-        eje ="no existe"
+const recorrerEjercicios = (ejer) => {
+    let eje = exercises2[ejer]
+        ? exercises2[ejer].map((exer) => {
+              return exer.nombre;
+          })
+        : (eje = "no existe");
 
-    return eje
+    return eje;
     // exercises2[ejer].map( exercise => {
     //     console.log(exercise.nombre)
     // })
-}
+};
 
-const $container = document.getElementById('container')
-const $week = document.getElementById('week')
-const $pierna = document.getElementById('pierna')
+const $container = document.getElementById("container");
+const $week = document.getElementById("week");
+const $pierna = document.getElementById("pierna");
 
-$pierna.addEventListener('click', () =>{
-    console.log(recorrerEjercicios($pierna.value))
-})
+$pierna.addEventListener("click", () => {
+    console.log(recorrerEjercicios($pierna.value));
+});
 
 const weekDay = () => {
-    let daysText = ''
+    let daysText = "";
     let date = new Date();
-    let numDay = date.getDate()
-    const dias = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    let numDay = date.getDate();
+    const dias = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     for (let i = 0; i < dias.length; i++) {
         const dia = dias[i % 7];
-        if(date.getDay() == i){
+        if (date.getDay() == i) {
             daysText += `
                 <div class='day-cont'>
                     <a class='day special'>${dia}</a>
                     <p>${numDay}</p>
                 </div>
-                `
+                `;
         } else
             daysText += `
                 <div class='day-cont'>
                     <a class='day'>${dia}</a>
                     <p>${numDay}</p>
                 </div>
-                `
+                `;
     }
-    $week.innerHTML = daysText
-    return date.getDay()
-}
+    $week.innerHTML = daysText;
+    return date.getDay();
+};
 
-
-function showExercises(){
-    weekDay()
-    let theExercises = ""
-    if(weekDay() == 0){
-        const legs = exercises.filter(exercise => exercise.type == 'legs')
-        legs.map(exercise => {
-            theExercises += 
-            `
+function showExercises() {
+    weekDay();
+    let theExercises = "";
+    if (weekDay() == 0) {
+        const legs = exercises.filter((exercise) => exercise.type == "legs");
+        legs.map((exercise) => {
+            theExercises += `
                 <div class="col s12 m7">
                     <div class="card">
                         <div class="card-image">
@@ -84,10 +106,8 @@ function showExercises(){
                         </div>
                     </div>
                 </div>
-            `
-        })
+            `;
+        });
     }
-    $container.innerHTML = theExercises
+    $container.innerHTML = theExercises;
 }
-
-
