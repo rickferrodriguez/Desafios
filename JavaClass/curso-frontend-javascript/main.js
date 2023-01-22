1339045926c55a60c4eb282c2a51bbdc118f6090
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-22 14:54:35 -0500

    detalle de la lista de productos

--- JavaClass/curso-frontend-javascript/main.js
+++ JavaClass/curso-frontend-javascript/main.js
@@ -7,28 +7,33 @@ const $product_detail = document.querySelector('.product-detail')
 const $cards_container = document.querySelector('.cards-container')
 const $clothes_el = document.querySelector('#clothes-el')
 const $toys_el = document.querySelector('#toys-el')
+const $product_detail_onc = document.querySelector('.product-detail-onc')
 
-const product = {
+const products = {
     toys : [
         {
+            "id": 0,
             "nombre": "bike",
             "precio": 120,
             "image": "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             "cantidad": 4,
         },
         {
+            "id": 1,
             "nombre": "bike",
             "precio": 120,
             "image": "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             "cantidad": 4,
         },
         {
+            "id": 2,
             "nombre": "bike",
             "precio": 120,
             "image": "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             "cantidad": 4,
         },
         {
+            "id": 3,
             "nombre": "Adult Trike",
             "precio": 220,
             "image": "https://images.pexels.com/photos/957157/pexels-photo-957157.jpeg?auto=compress&cs=tinysrgb&w=650&h=940&dpr=1",
@@ -37,30 +42,35 @@ const product = {
     ],
     clothes : [
         {
+            "id": 0,
             "nombre": "shirt",
             "precio": 20,
             "image": "https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             "cantidad": 4,
         },
         {
+            "id": 1,
             "nombre": "shirt",
             "precio": 20,
             "image": "https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             "cantidad": 4,
         },
         {
+            "id": 2,
             "nombre": "shirt",
             "precio": 20,
             "image": "https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             "cantidad": 4,
         },
         {
+            "id": 3,
             "nombre": "shirt",
             "precio": 20,
             "image": "https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             "cantidad": 4,
         },
         {
+            "id": 4,
             "nombre": "skirt",
             "precio": 22,
             "image": "https://images.pexels.com/photos/957157/pexels-photo-957157.jpeg?auto=compress&cs=tinysrgb&w=650&h=940&dpr=1",
@@ -87,11 +97,11 @@ $shopping_button.addEventListener('click', () => {
     $desktop_menu.classList.add('inactive')
 })
 
-const renderBikes = (arra) => {
+const renderProduct = (arra) => {
     let textInner = ''
-    product[arra].map( ele => {
+    products[arra].map( ele => {
             textInner += `
-                <div class="product-card">
+                <div class="product-card" onclick="cardClick(${ele.id})">
                     <img src="${ele.image}" alt="">
                     <div class="product-info">
                         <div>
@@ -106,17 +116,45 @@ const renderBikes = (arra) => {
             `
     })
     $cards_container.innerHTML = textInner
+    return arra
 }
 
+// creado para saber en que ventana de productos me encuentro
+let presionando = ''
+
+// retorna el nombre de la ventana en la que me encuentro
 $clothes_el.addEventListener('click', (event) =>{
     const value = event.target.getAttribute('data-value')
-    renderBikes(value)
+    presionando = value
+    renderProduct(value)
 })
 
+// retorna el nombre de la ventana en la que me encuentro
 $toys_el.addEventListener('click', (event) =>{
     const value = event.target.getAttribute('data-value')
-    renderBikes(value)
+    presionando = value
+    renderProduct(value)
 })
 
+const cardClick = (valor) => {
+    // un array del objeto presionado
+    let productArray = products[renderProduct(presionando)]
+    let indice = productArray.findIndex((product) => product.id === valor);
+    $product_detail_onc.innerHTML = `
+            <div class="product-detail-onc-close">
+                <img src="./icons/icon_close.png" alt="close">
+            </div>
+            <img src="${productArray[indice].image}" alt="bike">
+            <div class="product-info-onc">
+                <p>${productArray[indice].precio}</p>
+                <p>${productArray[indice].nombre}</p>
+                <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+                <button class="primary-button add-to-cart-button">
+                    <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+                        Add to cart
+                </button>
+            </div>
+    `
+}
 
 
