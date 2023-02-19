11441b3d55a9889e322ca94ed1571ab56aeb4cbd
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-18 21:39:10 -0500

    fetching the youtube midudev videos

--- JavaClass/proyectoAsincronismo/src/assets/main.js
+++ JavaClass/proyectoAsincronismo/src/assets/main.js
@@ -1,4 +1,6 @@
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC8LeXCWOalN8SxlrPcG-PaQ&part=snippet%2Cid&order=date&maxResults=9';
+const $vid_container = document.querySelector('#vid-container')
+const $vid_thumbnail = document.querySelector('#vid-thumbnail')
 
 
 const options = {
@@ -17,7 +19,30 @@ async function fetchData(urlApi) {
 
 const recorrido = async (url) => {
     let data = await fetchData(url)
-    console.log(data)
+    let items = data.items
+    let texto = ''
+    items.forEach((item) => {
+        Object.entries(item).map((entry, index, arr) => {
+            const [key, value] = entry
+            if(key === 'snippet') {
+                // texto += `<p>${value.description}</p>`
+                console.log(value)
+                texto += `
+                    <div class="card">
+                        <picture id="vid-thumbnail">
+                            <img src="${value.thumbnails.high.url}" alt="">
+                        </picture>
+                        <div class="card-content">
+                            <h3>${value.title}</h3>
+                            <p>${value.description}</p>
+                        </div>
+                    </div>
+`
+            }
+        })
+    })
+    $vid_container.innerHTML = texto
+    // $vid_thumbnail.innerHTML = texto
 }
 
 recorrido(API)
