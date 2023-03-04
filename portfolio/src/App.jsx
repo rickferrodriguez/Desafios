3a38ecdd11c9305cec8eb48fd13d8dd39cba01d0
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-03 22:33:19 -0500

    header done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import sportLogo from './assets/sport.svg'
 import './App.css'
 import {Header} from './components/Header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header></Header>
       <main>
         <section>
-          <img className="logo main" src={sportLogo} alt="React logo" />
           <h1>Hi I'm Richard</h1>
         </section>
       </main>
