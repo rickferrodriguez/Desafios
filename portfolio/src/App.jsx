0d51856a3cdf30d2077988f4c002219b30e3d681
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-03 22:46:47 -0500

    main text and the projects title completed

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,8 +7,12 @@ function App() {
     <>
       <Header></Header>
       <main>
-        <section>
-          <h1>Hi I'm Richard</h1>
+        <section className="home-main-content">
+          <h1>Hi I'm RickFeR <span className="emoji-header">🏋🏼‍♂️</span></h1>
+          <p className="home-principal-text">Desarrollador full-stack que construye su propia versión de un mundo digital.</p>
+        </section>
+        <section className="projects">
+          <h2>PROJECTS</h2>
         </section>
       </main>
     </>
