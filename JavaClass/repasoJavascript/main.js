67085d128e74d93da50c040bdaa29fdc4f4c7147
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-29 16:49:55 -0500

    getting movies, TODO: filters

--- JavaClass/repasoJavascript/main.js
+++ JavaClass/repasoJavascript/main.js
@@ -2,23 +2,52 @@ import './style.css'
 
 const PREFIX_MOVIES = 'http://www.omdbapi.com/?apikey=5a03f14a'
 
+const form = document.querySelector('#formMovie')
+const log = document.querySelector('#log')
+const filters = document.querySelector('#filters')
+
 const getMovies = async (event) => {
   event.preventDefault()
-  const search = event.target.search.value
-  let movies = []
+  const data = Object.fromEntries(
+    new FormData(event.target)
+  )
+
+  if (data.search === ' ') return null
+
   try {
-    const response = await fetch(`${PREFIX_MOVIES}&s=${search}`)
+    const response = await fetch(`${PREFIX_MOVIES}&s=${data.search}`)
     const json = await response.json()
-    movies = json
+    const movies = json.Search
+    const mappedMovies = movies?.map(m => ({
+      id: m.imdbID,
+      title: m.Title,
+      poster: m.Poster,
+      year: m.Year,
+      type: m.Type
+    }))
+
+    showMovies({ mappedMovies })
   } catch (e) {
     throw new Error('Failed to get movies')
   }
-  log.textContent = movies.Search[0].Title
-  console.log(movies.Search)
-  return movies
 }
 
-const form = document.querySelector('#formMovie')
-const log = document.querySelector('#log')
+async function showMovies ({ mappedMovies }) {
+  const myMovies = await mappedMovies
+  myMovies
+    ? log.innerHTML = `
+      <ul class='movies'>
+        ${mappedMovies.map(movies => {
+          return `
+        <li class='movie'>
+          <h3>${movies.title}</h3>
+          <img src=${movies.poster} alt=${movies.title} />
+          <p>${movies.year}</p>
+        </li>`
+        }).join('\n')}
+      </ul>
+      `
+    : log.innerText = 'no hay peliculas'
+}
 
 form.addEventListener('submit', getMovies)
